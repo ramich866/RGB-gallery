39b9b310eb584b5bbadcff37f6bc98040f186fca
ramich866 <ramichhonor@gmail.com>
2025-03-17 00:12:40 +0200

    add client folders

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,3 @@
-import Profile from "./pages/Profile.jsx";
-import Home from "./pages/Home.jsx";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Products from "./pages/Products.jsx";
-import Counter from "./pages/Counter.jsx";
-import "./styles/App.css";
-
-const App = () => {
-  return (
-    <BrowserRouter>
-      {/* <nav>This is my navbar</nav> */}
-
-      <Routes>
-        <Route path="/home" element={<Home />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/counter" element={<Counter />} />
-
-        <Route path="/*" element={<h1>Not Found</h1>} />
-      </Routes>
-    </BrowserRouter>
-  );
-};
-
-export default App;
+import React from 'react';
+import {BrowserRouter, Route , Routes} from "react-router-dom";
+import Login from './pages/Login';
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,9 @@
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./styles/index.css";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
 
-const element = document.getElementById("root");
-
-const root = ReactDOM.createRoot(element);
-
-root.render(<App />);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
