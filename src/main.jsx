03a34890693360098eba188c32327d2f78638280
ramich866 <ramichhonor@gmail.com>
2025-03-16 23:19:50 +0200

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./styles/index.css";
+
+const element = document.getElementById("root");
+
+const root = ReactDOM.createRoot(element);
+
+root.render(<App />);
